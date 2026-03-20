d199505d0ec8a0ae2310bd45d02a0c662240ad65
Pedro Carrero <161247437+Carrero007@users.noreply.github.com>
2026-03-20 08:02:04 -0300

    Delete agr vai directory

--- agr vai/AgroScan/AgroScan/Models/Login.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AgroScan.Models
-{
-    public class Login
-    {
-        public string CPF { get; set; } = string.Empty;
-        public string Senha { get; set; } = string.Empty;
-    }
-}
